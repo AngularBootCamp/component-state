c430659b9c290f8d37a30854cdb2dd816f67c7df
Kyle Cordes <kyle.cordes@oasisdigital.com>
2018-08-08 11:54:02 -0500

    curriculum update

--- src/app/do/do.module.ts
+++ src/app/do/do.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { SharedModule } from '../shared-module/shared.module';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
-import { PositionListComponent } from './position-list/position-list.component';
 
 import { DoComponent } from './do.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { PositionListComponent } from './position-list/position-list.component';
 
 const doRoutes: Routes = [
   { path: '', component: DoComponent }
